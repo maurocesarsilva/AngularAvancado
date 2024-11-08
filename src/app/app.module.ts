722a2c852b829f9408a33b7553db3a5f618b31c2
maurocesarsilva <46461097+maurocesarsilva@users.noreply.github.com>
2024-11-08 16:46:35 -0300

    validações basicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
+import { CustomFormsModule } from 'ng2-validation'
+
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
@@ -25,6 +27,7 @@ import { CadastroComponent } from './demos/reactiveFroms/cadastro/cadastro.compo
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    CustomFormsModule,
     [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
   ],
   providers: [
--- src/app/demos/reactiveFroms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveFroms/cadastro/cadastro.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder,FormControl, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from './models/usuario';
+
+// precisa de instalação
+import { CustomValidators } from 'ng2-validation';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,23 +12,33 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 export class CadastroComponent implements OnInit {
 
   cadastroForm: FormGroup
+  usuario: Usuario; 
+  formResult: string = '';
 
   constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
     // add ao html a prop cadastroForm
-    this.cadastroForm = this.fb.group(
-      {
-        nome: [''],
-        cpf: [''],
-        email: [''],
-        senha: [''],
-        senhaConfirmacao: [''],
-      }
-    );
+    let senha = new FormControl('', [Validators.required, CustomValidators.rangeLength([6,15])]);
+    let senhaConfirm = new FormControl('', [Validators.required, CustomValidators.rangeLength([6,15]), CustomValidators.equalTo(senha)]);
+
+    this.cadastroForm = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(150)]],
+      cpf: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: senha,
+      senhaConfirmacao: senhaConfirm
+    });
   }
 
   adicionarUsuario() {
-    let x = this.cadastroForm.value;
+    if (this.cadastroForm.dirty && this.cadastroForm.valid) {
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+       this.formResult = JSON.stringify(this.cadastroForm.value);
+    }
+    else {
+       this.formResult = "Não submeteu!!!"
+    }
+
   }
 }
