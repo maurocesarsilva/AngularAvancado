9481053d0ee5f4b145eb9a06ab023264968df2f5
maurocesarsilva <46461097+maurocesarsilva@users.noreply.github.com>
2024-11-08 13:58:42 -0300

    projeto curso

---
+++ front-end/01 - Template Bootstrap/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { NavegacaoModule } from './navegacao/navegacao.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    NavegacaoModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
