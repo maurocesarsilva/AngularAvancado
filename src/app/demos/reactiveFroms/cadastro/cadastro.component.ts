9b64f2da9e3b662ed9c962f92ac9f8f54ec0f0bf
maurocesarsilva <46461097+maurocesarsilva@users.noreply.github.com>
2024-11-08 15:23:17 -0300

    FormBuilder e FormGroup

--- src/app/demos/reactiveFroms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveFroms/cadastro/cadastro.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  templateUrl: './cadastro.component.html',
-  styles: []
+  templateUrl: './cadastro.component.html'
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  cadastroForm: FormGroup
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
+    // add ao html a prop cadastroForm
+    this.cadastroForm = this.fb.group(
+      {
+        nome: [''],
+        cpf: [''],
+        email: [''],
+        senha: [''],
+        senhaConfirmacao: [''],
+      }
+    );
   }
 
+  adicionarUsuario() {
+    let x = this.cadastroForm.value;
+  }
 }
